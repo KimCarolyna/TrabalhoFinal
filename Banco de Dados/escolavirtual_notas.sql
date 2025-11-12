12ca5f6d4db773410d4b0b06d146815a71a3395e
Felipe Campos Hipólito <felipecamhipolito@gmail.com>
2025-11-12 14:34:00 -0300

    Add columns N1, N2, N3 to notas table

--- Banco de Dados/escolavirtual_notas.sql
+++ Banco de Dados/escolavirtual_notas.sql
@@ -1,6 +1,8 @@
 CREATE TABLE notas (
     id_nota INT AUTO_INCREMENT PRIMARY KEY,
     id_matricula INT NOT NULL,
-    nota DECIMAL(5,2) NOT NULL,
+    N1 DECIMAL(5,2),
+    N2 DECIMAL(5,2),
+    N3 DECIMAL(5,2),
     FOREIGN KEY (id_matricula) REFERENCES matriculas(id_matricula)
 );
