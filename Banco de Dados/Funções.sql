e3519da7dbfa51906d18c3e019746b32aec00ae1
Felipe Campos Hipólito <felipecamhipolito@gmail.com>
2025-11-13 14:31:15 -0300

    Update Funções.sql

--- Banco de Dados/Funções.sql
+++ Banco de Dados/Funções.sql
@@ -8,6 +8,8 @@ BEGIN
 END;
 
 
+
+
 DELIMITER //
 CREATE PROCEDURE inserir_nota(
     IN p_id_matricula INT,
@@ -18,5 +20,22 @@ CREATE PROCEDURE inserir_nota(
 BEGIN
     INSERT INTO notas (id_matricula, N1, N2, N3, media)
     VALUES (p_id_matricula, p_N1, p_N2, p_N3, (p_N1 + p_N2 + p_N3)/3);
+END //
+DELIMITER ;
+
+
+
+
+DELIMITER //
+CREATE FUNCTION gerar_id_usuario()
+RETURNS VARCHAR(10)
+DETERMINISTIC
+BEGIN
+    DECLARE novo_id VARCHAR(10);
+    DECLARE ultimo INT;
+
+    SELECT IFNULL(MAX(id_usuario), 0) + 1 INTO ultimo FROM usuarios;
+    SET novo_id = CONCAT('USR', LPAD(ultimo, 4, '0'));
+    RETURN novo_id;
 END //
 DELIMITER ;
