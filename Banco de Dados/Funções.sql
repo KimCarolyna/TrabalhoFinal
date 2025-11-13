dafb644e67b1f1ddf4b5fea6f56dd463d6b5469d
Felipe Campos Hipólito <felipecamhipolito@gmail.com>
2025-11-13 14:14:41 -0300

    Create Funções.sql

---
+++ Banco de Dados/Funções.sql
@@ -0,0 +1,8 @@
+CREATE FUNCTION calcular_media(matricula_id INT)
+RETURNS DECIMAL(5,2)
+DETERMINISTIC
+BEGIN
+    DECLARE media DECIMAL(5,2);
+    SELECT (N1 + N2 + N3) / 3 INTO media FROM notas WHERE id_matricula = matricula_id;
+    RETURN media;
+END;
