2194c1456366bc893ece461d9ed4861ac50af1ab
Felipe Campos Hipólito <felipecamhipolito@gmail.com>
2025-11-13 14:28:01 -0300

    Adicionando uma procedure na tabela de função

--- Banco de Dados/Funções.sql
+++ Banco de Dados/Funções.sql
@@ -6,3 +6,17 @@ BEGIN
     SELECT (N1 + N2 + N3) / 3 INTO media FROM notas WHERE id_matricula = matricula_id;
     RETURN media;
 END;
+
+
+DELIMITER //
+CREATE PROCEDURE inserir_nota(
+    IN p_id_matricula INT,
+    IN p_N1 DECIMAL(5,2),
+    IN p_N2 DECIMAL(5,2),
+    IN p_N3 DECIMAL(5,2)
+)
+BEGIN
+    INSERT INTO notas (id_matricula, N1, N2, N3, media)
+    VALUES (p_id_matricula, p_N1, p_N2, p_N3, (p_N1 + p_N2 + p_N3)/3);
+END //
+DELIMITER ;
