4e84b3ba0d084f3a85d368e108be0661af86a432
Felipe Campos Hipólito <felipecamhipolito@gmail.com>
2025-11-13 14:04:14 -0300

    Delete Banco de Dados/escolavirtual_notas.sql

--- Banco de Dados/escolavirtual_notas.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE notas (
-    id_nota INT AUTO_INCREMENT PRIMARY KEY,
-    id_matricula INT NOT NULL,
-    N1 DECIMAL(5,2),
-    N2 DECIMAL(5,2),
-    N3 DECIMAL(5,2),
-    FOREIGN KEY (id_matricula) REFERENCES matriculas(id_matricula)
-);
